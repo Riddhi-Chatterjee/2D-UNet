16fd062eb6e6497ee33090ccb77d44b8c32f6ad0
Riddhi-Chatterjee <79003900+Riddhi-Chatterjee@users.noreply.github.com>
2023-12-01 15:18:58 +0530

    Add files via upload

---
+++ unet.py
@@ -0,0 +1,89 @@
+import math
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+__all__ = ['unet']
+
+def double_convolution(in_channels, out_channels):
+    
+    conv_op = nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels, affine=False, track_running_stats=False),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels, affine=False, track_running_stats=False),
+        nn.ReLU(inplace=True)
+    )
+    return conv_op
+
+class unet(nn.Module):
+    def __init__(self, inChannels, outChannels):
+        super(unet, self).__init__()
+        self.max_pool2d = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.down_convolution_1 = double_convolution(inChannels, 64)
+        self.down_convolution_2 = double_convolution(64, 128)
+        self.down_convolution_3 = double_convolution(128, 256)
+        self.down_convolution_4 = double_convolution(256, 512)
+        self.down_convolution_5 = double_convolution(512, 1024)
+
+        # Expanding path.
+        self.up_transpose_1 = nn.ConvTranspose2d(
+            in_channels=1024, out_channels=512,
+            kernel_size=2,
+            stride=2)
+        # Below, `in_channels` again becomes 1024 as we are concatinating.
+        self.up_convolution_1 = double_convolution(1024, 512)
+        self.up_transpose_2 = nn.ConvTranspose2d(
+            in_channels=512, out_channels=256,
+            kernel_size=2,
+            stride=2)
+        self.up_convolution_2 = double_convolution(512, 256)
+        self.up_transpose_3 = nn.ConvTranspose2d(
+            in_channels=256, out_channels=128,
+            kernel_size=2,
+            stride=2)
+        self.up_convolution_3 = double_convolution(256, 128)
+        self.up_transpose_4 = nn.ConvTranspose2d(
+            in_channels=128, out_channels=64,
+            kernel_size=2,
+            stride=2)
+        self.up_convolution_4 = double_convolution(128, 64)
+        # output => `out_channels`
+        self.out = nn.Conv2d(
+            in_channels=64, out_channels=outChannels,
+            kernel_size=1
+        )
+
+    def forward(self, x):
+        down_1 = self.down_convolution_1(x)
+        down_2 = self.max_pool2d(down_1)
+        down_3 = self.down_convolution_2(down_2)
+        down_4 = self.max_pool2d(down_3)
+        down_5 = self.down_convolution_3(down_4)
+        down_6 = self.max_pool2d(down_5)
+        down_7 = self.down_convolution_4(down_6)
+        down_8 = self.max_pool2d(down_7)
+        down_9 = self.down_convolution_5(down_8)
+
+        up_1 = self.up_transpose_1(down_9)
+        up_2 = self.up_convolution_1(torch.cat([down_7, up_1], 1))
+        up_3 = self.up_transpose_2(up_2)
+        up_4 = self.up_convolution_2(torch.cat([down_5, up_3], 1))
+        up_5 = self.up_transpose_3(up_4)
+        up_6 = self.up_convolution_3(torch.cat([down_3, up_5], 1))
+        up_7 = self.up_transpose_4(up_6)
+        up_8 = self.up_convolution_4(torch.cat([down_1, up_7], 1))
+
+        out = self.out(up_8)
+
+        return out
+    
+if __name__ == '__main__':
+    net = unet(inChannels=3, outChannels=3)
+    x = Variable(torch.FloatTensor(16, 3, 240, 240))
+    y = net(x)
+    print(y.data.shape) #Output shape for this input --> torch.Size([16, 3, 240, 240])
